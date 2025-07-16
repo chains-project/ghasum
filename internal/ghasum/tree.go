af58c0aa73820c9f1780faa8811e7b172ad2b4fd
Eric Cornelissen <ericornelissen@gmail.com>
2025-07-16 11:16:08 +0200

    Improve `ghasum list` to list dependencies hierarchically

---
+++ internal/ghasum/tree.go
@@ -0,0 +1,85 @@
+// Copyright 2025 Eric Cornelissen
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ghasum
+
+import (
+	"fmt"
+	"iter"
+	"slices"
+	"strings"
+
+	"github.com/chains-project/ghasum/internal/gha"
+)
+
+type tree struct {
+	value    *gha.GitHubAction
+	children []*tree
+}
+
+func (t *tree) add(c *tree) {
+	t.children = append(t.children, c)
+}
+
+func (t *tree) All() iter.Seq[gha.GitHubAction] {
+	return func(yield func(gha.GitHubAction) bool) {
+		_ = t.every(yield)
+	}
+}
+
+func (t *tree) every(f func(gha.GitHubAction) bool) bool {
+	if t.value != nil && !f(*t.value) {
+		return false
+	}
+
+	for _, child := range t.children {
+		if !child.every(f) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (t *tree) String() string {
+	var b strings.Builder
+
+	root := t.value == nil
+	if !root {
+		name := fmt.Sprintf("%s (%s)", t.value, t.value.Kind)
+		b.WriteString(name)
+		b.WriteRune('\n')
+	}
+
+	ordered := make([]string, len(t.children))
+	mapped := make(map[string]*tree, len(t.children))
+	for i, child := range t.children {
+		id := child.value.String()
+		ordered[i] = id
+		mapped[id] = child
+	}
+
+	slices.SortFunc(ordered, strings.Compare)
+	for _, name := range ordered {
+		child := mapped[name]
+		for line := range strings.Lines(child.String()) {
+			if !root {
+				b.WriteString("  ")
+			}
+			b.WriteString(line)
+		}
+	}
+
+	return b.String()
+}
